83b47e25f23cf7efca34b85e3ef9f636fb58975a
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-07-17 12:45:02 +0530

    Check if a number is ARMSTRONG or NOT

---
+++ Ckeck_Armstrong.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int CountDigits(int num){
+    
+    int count = 0;
+
+    while(num != 0){
+        count ++;
+        num /= 10;
+    }
+
+    return count;
+}
+
+
+int CheckArmstrong(int num){
+    
+    int sum = 0;
+    int temp = num;
+    int lastdigit = 0;
+    int digits = CountDigits(num);
+
+    while(num != 0){
+        lastdigit = num % 10;
+        sum += pow(lastdigit,digits);
+        num /= 10;
+    }
+
+    if(temp == sum) return 1;
+    else {
+        return 0;
+    }
+}
+
+
+
+int main(){
+
+    int num;
+    cout << "Enter a number : ";
+    cin >> num;
+
+    int armstrong = CheckArmstrong(num);
+    
+    if (armstrong == 1) cout << "YES" ;
+    else cout << "NO";
+
+    return 0;
+}
