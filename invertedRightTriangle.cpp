65e4621100d4031edb2cdf9d2e0b8db35c28767b
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-02 18:29:59 +0530

    Inverted Right Triangle Pattern

---
+++ invertedRightTriangle.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the No. of Rows : ";
+    cin >> n;
+    cout << endl;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(j <= n + 1 - i ){
+                cout << "* ";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+
+
+* * * * * 
+* * * *   
+* * *     
+* *       
+*
+
+
+*/
