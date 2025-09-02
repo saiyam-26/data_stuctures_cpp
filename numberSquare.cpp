d9a34cf8d9e55f2f16ec2d938f42118ec8d16a5b
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-02 18:32:30 +0530

    Filled Number Square Pattern

---
+++ numberSquare.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the No. of Rows : ";
+    cin >> n;
+    cout << endl;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(true ){
+                cout << j;
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+12345
+12345
+12345
+12345
+12345
+*/
