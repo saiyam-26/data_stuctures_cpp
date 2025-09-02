a88f716321d0addfd31dbb9e0a197efe4401bdd8
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-02 18:31:32 +0530

    Numbered Hollow Square Pattern

---
+++ numberedHollowSquare.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the No. of Rows : ";
+    cin >> n;
+    cout << endl;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(i == 1 || i == n || j == 1 || j == n ){
+                cout << j;
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+12345
+1   5
+1   5
+1   5
+12345
+
+*/
