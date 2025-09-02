d674695427a7b4008a5eaadb39199c792e3fa022
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-02 18:26:30 +0530

    Hollow Numeric Pyramid Pattern

---
+++ hollowPyramid.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Enter the No. of Rows : ";
+    cin >> n;
+    cout << endl;
+    
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= 4 + n; j++){
+            if(j == n + 1 - i or j == n - 1 + i or i == n ){
+                cout << i;
+                
+            }else{
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+   1    
+  2 2   
+ 3   3  
+44444444
+*/
+
