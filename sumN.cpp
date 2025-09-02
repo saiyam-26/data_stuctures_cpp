9b45d9d57ab5b39e3031f99124906bd94cf383b9
SAIYAM GUPTA <saiyamsomya@gmail.com>
2025-09-02 19:29:20 +0530

    Sum of N terms using Recursion

--- sumN.cpp
+++ sumN.cpp
@@ -1,17 +1,15 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
+int sum(int n) {
+    if (n == 1) return 1;
+    return n + sum(n - 1);
+}
+
 int main() {
-    int n, sum = 0;
-
-    cout << "Enter a positive number: ";
+    int n;
+    cout << "Enter a number: ";
     cin >> n;
-
-    for (int i = 1; i <= n; i++) {
-        sum += i;
-    }
-
-    cout << "Sum of first " << n << " natural numbers is: " << sum << endl;
-
+    cout << "Sum = " << sum(n);
     return 0;
 }
